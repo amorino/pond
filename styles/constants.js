00dfd0ffe8edee593ad223a9c434f9baa4c4a575
Erick Bazán <erick.bazan@me.com>
2019-09-24 17:39:36 -0500

    chore(app): add styled-components

---
+++ styles/constants.js
@@ -0,0 +1,40 @@
+// @flow
+import { css } from 'styled-components';
+
+type BrowserSizes = {
+  desktop: number,
+  tablet: number,
+  mobile: number,
+};
+
+export const sizes: BrowserSizes = {
+  desktop: 992,
+  tablet: 768,
+  mobile: 609,
+};
+
+export const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args: any) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `;
+  return acc;
+}, {});
+
+export const colors = {
+  white: '#fff',
+  black: '#000',
+  red: '#ff0000',
+  grey: '#cccccc',
+};
+
+export const provider = {
+  breakpoints: [`${sizes.mobile}`, `${sizes.tablet}px`, `${sizes.desktop}px`],
+  space: [0, 6, 12, 18, 24],
+  fontSizes: [12, 14, 16, 24, 36, 72],
+};
+
+export const fontFamily = {
+  gotham: 'Gotham, sans-serif',
+};
---
+++ styles/global.js
@@ -0,0 +1,28 @@
+// @flow
+import { injectGlobal } from 'styled-components';
+import { colors, fontFamily } from './constants';
+
+injectGlobal`
+  body, html {
+    background-color: ${colors.white};
+    width: 100%;
+    height: 100%;
+    min-width: 100%;
+    min-height: 100%;
+    font-family: ${fontFamily.gotham};
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    color: ${colors.black};
+  }
+  button {
+    border: none;
+    padding: 0;
+    margin: 0;
+    cursor: pointer;
+    font-family: inherit;
+    border-radius: 0;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
